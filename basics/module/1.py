8a86eb087abe61d94b53eeaeaef9ba195b51d930
jong in <bill1224@naver.com>
2020-10-05 16:57:28 +0900

    10/05 module

---
+++ basics/module/1.py
@@ -0,0 +1,6 @@
+import math #math라는 module을 불러온다.
+#math 수학과 관련된 함수들은 math라는 module에 있다.
+print(math.ceil(3.1))
+print(math.floor(3.9))
+print(math.sqrt(16))
+print(math.factorial(3))
---
+++ basics/module/3.py
@@ -0,0 +1,14 @@
+from egoing import a
+#from egoing import a as k -> 함수 a를 k 로 변경해서 사용한다.
+import bill1224
+#import bill1224 as k -> module명을 k로 변경해서 사용
+#print(k.a())
+print(a())
+print(bill1224.a())
+
+
+
+# print(bill1224.a())
+
+#함수 이름이 중복될 수 있다.
+#이런 것을 해결하기 위해서 module을 사용하기도 한다.
