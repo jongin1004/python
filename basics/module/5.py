49e86eceedc4f1d39942e1ba61f2b59fdda0a946
jong in <bill1224@naver.com>
2020-10-06 23:48:30 +0900

    10/06 object oriented programming

---
+++ basics/module/5.py
@@ -0,0 +1,6 @@
+import auth
+input_id = input("아이디를 입력하세요.\n")
+if auth.login(input_id):
+    print('hello, '+input_id)
+else:
+    print('who are you')
---
+++ basics/module/auth.py
@@ -0,0 +1,6 @@
+def login(_id):
+    members = ['egoing', 'bill1224']
+    for member in members:
+        if member == _id:
+            return True
+    return False
