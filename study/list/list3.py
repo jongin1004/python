7c99b0ade3571f274f2b4b833717bd266237260d
jong in <bill1224@naver.com>
2020-11-09 23:34:41 +0900

    11/09 list

---
+++ study/list/list3.py
@@ -0,0 +1,17 @@
+apart = [ [101, 102], [201, 202], [301, 302] ]
+
+for row in apart:
+    for col in row:
+        print(col, "room")
+        print("-" * 5)
+
+for row in apart:
+    for col in row:
+        print(col, "room")
+    print("-----")
+
+
+for row in apart:
+    for col in row:
+        print(col, "room")
+print("-" * 5)
