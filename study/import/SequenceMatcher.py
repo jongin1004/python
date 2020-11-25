d16667959bda89cb4efb66c9df20acd70d32246d
jong in <bill1224@naver.com>
2020-11-25 15:31:57 +0900

    ...

--- study/import/SequenceMatcher.py
+++
@@ -1,7 +0,0 @@
-from difflib import SequenceMatcher
-
-str1 = 'abc'
-str2 = 'abc'
-
-ratio = SequenceMatcher(None, str1, str2).ratio()
-print(ratio)
--- study/sorted/sorted.py
+++
@@ -1,5 +0,0 @@
-string_list = ["a", "b", "d", "c"]
-print(sorted(string_list))
-
-int_list = [1,5,2,3,4]
-print(sorted(int_list))
