e0687933ac535a9a1990f0219bbac6f86903642e
jongin <bill1224@naver.com>
2020-12-10 12:02:39 +0900

    12/10 built-in, external funtion

---
+++ study/fx/built-in.py
@@ -0,0 +1,16 @@
+# #input : 사용자 입력을 받는 함수
+# languge = input("무슨 언어를 좋아하세요?")
+# print("{0}은 아주 좋은 언어입니다.".format(languge))
+
+# dir : 어떤 객체를 넘겨줬을 때 그 객체가 어떤 변수와 함수를 가지고 있는지 표시
+# import random  # 외장 함수
+# print(dir(random))
+
+# lst = [1, 2, 3]
+# print(dir(lst))
+
+# name = "jong"
+# print(dir(name))
+
+
+# list of python builtins를 구글에 검색하면 내장함수를 볼 수있는 사이트가 있음
