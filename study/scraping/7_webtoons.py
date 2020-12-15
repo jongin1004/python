9b6250ea22792fc32a635807048eb68872d8c3c5
jongin <bill1224@naver.com>
2020-12-15 11:47:10 +0900

    12/15 webtoons scrap

---
+++ study/scraping/7_webtoons.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://comic.naver.com/webtoon/weekday.nhn"
+res = requests.get(url)
+res.raise_for_status()  # 문제있을 경우 프로그램 종료
+
+soup = BeautifulSoup(res.text, "lxml")
+
+# 네이버 웹툰 목록 전체 가져오기
+cartoons = soup.find_all("a", attrs={"class": "title"})
+# find_all : class가 title인  모든 "a" element를 반환
+for cartoon in cartoons:
+    print(cartoon.get_text())
